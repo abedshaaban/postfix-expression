b8f684b73a02a0a47589abddba77a0056b10b63f
Abed Al Ghani Shaaban <abedshaaban600@gmail.com>
2023-11-27 19:35:53 +0200

    manipulate child in tree

--- tree.js
+++ tree.js
@@ -16,7 +16,7 @@ class Node {
     return this.value;
   }
 
-  getChildren() {
+  getChild() {
     return this.children;
   }
 
@@ -53,11 +53,43 @@ class Tree {
     this.children = [];
   }
 
+  getChild() {
+    return this.children;
+  }
+
+  getChildByID(id) {
+    if (this.root.id === id) {
+      return this.children;
+    } else {
+      for (let i = 0; i < this.children.length; i++) {
+        const childNode = this.children[i];
+
+        if (childNode.id === id) {
+          return childNode.getChild();
+        }
+      }
+    }
+  }
+
   insertChild(node) {
     if (this.root === null) {
       this.root = node;
     } else {
       this.children.push(node);
+    }
+  }
+
+  insertChildByID(id, node) {
+    if (this.root.id === id) {
+      this.children.push(node);
+    } else {
+      for (let i = 0; i < this.children.length; i++) {
+        const childNode = this.children[i];
+
+        if (childNode.id === id) {
+          childNode.insertChild(node);
+        }
+      }
     }
   }
 }
@@ -81,6 +113,13 @@ async function getInput() {
 // }
 
 const nodeElement = new Tree();
-// nodeElement.insertChild(new Node(6));
-console.log(nodeElement);
+
+const nodex = new Node(12);
+
+nodeElement.insertChild(new Node(6));
+nodeElement.insertChild(new Node(9));
+nodeElement.insertChild(nodex);
+nodeElement.insertChild(new Node(15));
+nodeElement.insertChildByID(nodex.getID(), new Node(21));
+console.log(nodeElement.getChildByID(nodex.getID()));
 // }
